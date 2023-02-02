e2ef573cc66ce8b230344859e1467813f4e89a07
Hyung Jin Kim <hyungjinkim1205@gmail.com>
2023-02-02 00:42:28 -0800

    add the function to add veggie toppings to the total price

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 //Business logic
-function Pizza(size, meatTopping,/*veggieTopping*/) {
+function Pizza(size, meatTopping, veggieTopping) {
     this.basePrice = 10.99;
     this.size = size;
     this.meatTopping = meatTopping;
-    //this.veggieTopping = veggieTopping;
+    this.veggieTopping = veggieTopping;
     this.extraFee = 0;
 }
 
@@ -23,14 +23,12 @@ Pizza.prototype.cost = function () {
     if (this.meatTopping.includes("ham") || this.meatTopping.includes("bacon")) {
         this.extraFee += 2;
     }
-    /*
     if(this.veggieTopping.includes("onion") || this.veggieTopping.includes("pineapple")) {
         this.extraFee += .75;
     }
     if(this.veggieTopping.includes("olive") || this.veggieTopping.includes("jalapeno")) {
         this.extraFee += 1.5;
     }
-    */
 }
 
 Pizza.prototype.total = function () {
@@ -44,10 +42,10 @@ function orderSubmit(event) {
     event.preventDefault();
     let sizeSelect = document.getElementById("pizza-size").value;
     let meatTopping = document.getElementById("meatTopping").value;
-    //let veggieTopping = document.getElementById("veggieTopping").value;
+    let veggieTopping = document.getElementById("veggieTopping").value;
     //let personInfor = document.getElementById("personInfo").value;
 
-    let pizzaOrder = new Pizza(sizeSelect, meatTopping)
+    let pizzaOrder = new Pizza(sizeSelect, meatTopping, veggieTopping)
     pizzaOrder.cost();
     let p = document.createElement("p");
     let body = document.querySelector("body");
